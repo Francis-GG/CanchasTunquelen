93f2da8d298eaf8939603f2b77188c59844e7e12
Francis-GG <francis.guerrero@outlook.com>
2024-07-17 00:57:51 -0400

    Profile progress 🐼

--- src/app/pages/Perfil/PerfilPage.tsx
+++ src/app/pages/Perfil/PerfilPage.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/react';
+import { IonAvatar, IonCard, IonContent, IonItem, IonLabel, IonText } from '@ionic/react';
+import './PerfilPage.css'
 
 const PerfilPage = () => (
     <>
         <IonContent>
-            <div
-                style={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    height: '100%',
-                }}
-            >
-                Contenido perfil
-            </div>
+            <IonCard className="profile-page">
+                <div className="flex justify-center items-center custom-avatar-image">
+                    <IonAvatar>
+                        <img src="/panda.png" alt="profile-image" />
+                    </IonAvatar>
+                </div>
+
+                <IonItem lines="none" className="profile">
+                    <IonLabel className="ion-text-center">
+                        <IonText color="secondary">
+                            Francisco
+                        </IonText>
+                        <p>
+                            <IonText color="medium">Casa 157</IonText>
+                        </p>
+                    </IonLabel>
+                </IonItem>
+            </IonCard>
         </IonContent>
     </>
 );
