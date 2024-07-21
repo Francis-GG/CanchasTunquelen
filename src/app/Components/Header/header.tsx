41a4b0d23edb0b4069c5b910265851adfa66b234
Francis-GG <francis.guerrero@outlook.com>
2024-07-21 09:10:48 -0400

    feat(loginPage)🔒: Login functions, header updated

--- src/app/Components/Header/header.tsx
+++ src/app/Components/Header/header.tsx
@@ -6,10 +6,12 @@ function Header() {
     return (
         <IonHeader>
             <IonToolbar>
-                <IonTitle>Canchas tunquelen</IonTitle>
-                <IonButton slot="end" shape="round" fill="clear">
-                    <IonIcon icon={settingsSharp} size="large" />
-                </IonButton>
+                <div className="flex items-center justify-between w-full">
+                    <img src="/header-logo.svg" alt="Header Logo" className="w-[80%] h-auto ml-3" />
+                    <IonButton slot="end" shape="round" fill="clear">
+                        <IonIcon icon={settingsSharp} size="large" />
+                    </IonButton>
+                </div>
             </IonToolbar>
         </IonHeader>
     );
